f397df146b58d97742f613e712c8a44a2103e564
essonani tchakpalla <essonani@hotmail.com>
2023-05-05 20:27:17 -0500

    switch

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter a number from 1 - 1000");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("number to low");
+
+            }
+            else if(userInput > favNumber) 
+            {
+                Console.WriteLine("number too high");
+            }
+            else 
+            {
+                Console.WriteLine("congratulation you guessed right");
+            }
+            // switch case exercise
+            Console.WriteLine("what is your favorite subject");
+            string subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("i hate math");
+                    break;
+                case "history":
+                    Console.WriteLine("history is boring");
+                    break;
+                case "science":
+                    Console.WriteLine("science is hard");
+                    break;
+                case "sport":
+                    Console.WriteLine("sport is cool");
+                    break;
+                case "spanish":
+                    Console.WriteLine("i dont care for it");
+                    break;
+                default: Console.WriteLine("i dont know that subject");
+                    break;
+            }
+
+
         }
-    }
-}
+        
+
+    }  
+}   
